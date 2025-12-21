71af479e755d1274b7f4fe51920679a0f4d07396
Febriyan Dwi Putra <febriyan96dwi@gmail.com>
2025-12-21 12:38:27 +0700

    edit hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,36 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "../../public/man-white.png";
 
 export default function Home() {
   return (
-    <div>
-      Home Page
+    <div className={styles.container}>
+      <div className={styles.textSection}>
+        <h1 className={styles.title}>
+          Building Smarter Webs with <br /> 
+          <span className={styles.highlight}>Data-Driven Insights</span>
+        </h1>
+        
+        <p className={styles.description}>
+          Exploring the intersection of <strong>Web Development</strong> and <strong>Data Science</strong>.
+        </p>
+        
+        <div className={styles.buttonGroup}>
+          <button className={styles.primaryBtn}>Explore My Projects</button>
+          <button className={styles.secondaryBtn}>Hire Me</button>
+        </div>
+      </div>
+
+      <div className={styles.imageSection}>
+        <div className={styles.heroWrapper}>
+          <Image 
+            src={Hero} 
+            alt="Data Visualization and Web Development Illustration"
+            priority // Bagus untuk LCP/performance gambar utama
+            layout="responsive"
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import styles from './footer.module.css'
+import Image from 'next/image';
 
 const Footer = () => {
   console.log("Footer component rendered");
     return (
     <div className={styles.container}>
         <div>© 2025 CodeFebs. All Rights Reserved</div>
-        <div></div>
+        <div className={styles.socials}>
+          <Image src="/1.png"  width={15} height={15} alt="CodeFebs-Facebook"/>
+          <Image src="/2.png"  width={15} height={15} alt="CodeFebs-Instagram" />
+          <Image src="/3.png"  width={15} height={15} alt="CodeFebs-Twitter / X"/>
+          <Image src="/4.png"  width={15} height={15} alt="CodeFebs-Youtube"/>
+        </div>
     </div>
   )
 }
